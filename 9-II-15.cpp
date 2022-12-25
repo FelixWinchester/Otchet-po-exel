9e43a6f431fd5d963c87d46f4ad0f649faa430cf
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2022-12-25 23:15:07 +0400

    Update 9-II-15.cpp

--- 9-II-15.cpp
+++ 9-II-15.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <algorithm>
 #include "fstream"
-
+#include <string>
 using namespace std;
 
-int main(){
-    ifstream in ("input.txt");
-    ofstream out ("output.txt");
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
     string s, s1;
     int x, x1;
-    while (in.peek() != EOF){
+    while (in.peek() != EOF) {
         getline(in, s);
         getline(in, s1);
         x = stoi(s);
