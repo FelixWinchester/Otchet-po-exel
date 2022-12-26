8afb035c006c216d39741c79840c4499f54a270f
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2022-12-27 01:05:27 +0400

    Update 7-II-2.cpp

--- 7-II-2.cpp
+++ 7-II-2.cpp
@@ -5,22 +5,23 @@ using namespace std;
 
 int main()
 {
-	string str, str1, str2;
-	string::size_type k = 0, pos = 0;
-	cout << "Enter string\n" << endl;
-	getline(cin, str);
-	cout << "Enter str1\n" << endl;
-	cin >> str1;
-	cout << "Enter str2\n" << endl;
-	cin >> str2;
-	k = str.find(str1, pos);
-	while (k != string::npos)
-	{
-		str.insert(k + 1, str2);
-		pos = k + 3;
-		k = str.find(str1, pos);
-	}
-	cout << "string=" << str << endl;
-	return 0;
+    string str, str1, str2;
+    string::size_type k = 0, pos = 0;
+    cout << "Enter string\n" << endl;
+    getline(cin, str);
+    cout << "Enter str1\n" << endl;
+    cin >> str1;
+    cout << "Enter str2\n" << endl;
+    cin >> str2;
+    k = str.find(str1, pos);
+    int j = str1.length();
+    while (k != string::npos)
+    {
+        str.insert(k + j, str2);
+        pos = k + j+3;
+        k = str.find(str1, pos);
+    }
+    cout << str << endl;
+    return 0;
 
 }
